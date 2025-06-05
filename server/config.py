0c5b77e8e4f3b80f1aed6ea79d4eb8149750e75d
GalenReed <galenreed@vt.edu>
2025-06-05 18:52:28 -0400

    started models

--- server/config.py
+++ server/config.py
@@ -8,12 +8,12 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-ma = Marshmallow(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -25,6 +25,9 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
+ma = Marshmallow(app)
 
 # Instantiate REST API
 api = Api(app)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,23 @@
-
-from config import db
+from config import db, bcrypt
 
 # Models go here!
 class User(db.Model):
     __tablename__= "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(100), nullable=False)
+
+    #Relationships
+    
+    @property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+    
